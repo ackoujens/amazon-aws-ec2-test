8e08ea40b59beba582978b722dde4fb0769c84ad
Ackou Jens <ackoujens@gmail.com>
2018-04-18 20:15:55 +0200

    Implement body parser

--- dist/volvo-valor/server/app.js
+++ dist/volvo-valor/server/app.js
@@ -1,5 +1,10 @@
 // Server root
+
+// Imports
 const express = require('express');
+const bodyParser = require('body-parser');
+
+// Express server
 const app = express();
 
 app.get('/api/user/login', (req, res) => {
@@ -7,3 +12,8 @@ app.get('/api/user/login', (req, res) => {
 })
 
 app.listen(3000, () => console.log('Blog server running on port 3000'));
+
+// Returns middleware that only parses json and urlencoded
+// Only looks at requests where Content-Type header matches the type option
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : false }));
